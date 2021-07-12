14f915d8908320893c9ef8422ec1313063ccf4d6
Weslin-0101 <70270543+Weslin-0101@users.noreply.github.com>
2021-07-11 22:28:27 -0300

    Commit inicial

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-dager"></i> por
+            <strong> Weslin <span className="text-danger">007</span></strong>
+        </span>
+    </footer>
